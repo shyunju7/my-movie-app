950c9d75f0bbc30eac3bfc932b89eb74b368fb52
shyunju7 <shyunju7@naver.com>
2021-08-17 17:03:11 +0900

    first commit

---
+++ my-movie-app/src/Components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RouterComponent from "./Router";
+
+function App() {
+  return (
+    <div>
+      <RouterComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-movie-app/src/Components/Router.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import TV from "./Routes/TV";
+
+const RouterComponent = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/tv" exact component={TV} />
+      <Route path="/search" component={Search} />
+      <Route
+        path="/tv/popular"
+        exact
+        render={() => {
+          <h2>popular TV show!</h2>;
+        }}
+      />
+      <Redirect from="*" to="/" />
+    </Switch>
+  </Router>
+);
+
+export default RouterComponent;
