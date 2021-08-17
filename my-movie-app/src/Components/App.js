7a364c8e9ebf6fbe04acff03787f8e74fe72a8b6
shyunju7 <shyunju7@naver.com>
2021-08-17 17:20:56 +0900

    Add Header

--- my-movie-app/src/Components/App.js
+++ my-movie-app/src/Components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "./Header";
 import RouterComponent from "./Router";
 
 function App() {
   return (
     <div>
+      <Header />
       <RouterComponent />
     </div>
   );
---
+++ my-movie-app/src/Components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <ul>
+        <li>
+          <a href="/">Movie</a>
+        </li>
+        <li>
+          <a href="/tv">TV</a>
+        </li>
+        <li>
+          <a href="/search">Search</a>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
