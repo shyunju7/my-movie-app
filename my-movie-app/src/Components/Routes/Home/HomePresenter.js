44060d865b60c4e5cc0ccec9cfc398f554c43141
shyunju7 <shyunju7@naver.com>
2021-08-23 15:53:45 +0900

    Add Section

--- my-movie-app/src/Components/Routes/Home/HomePresenter.js
+++ my-movie-app/src/Components/Routes/Home/HomePresenter.js
@@ -1,7 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+import Section from "Components/Section";
+
+const Container = styled.div`
+  padding: 10px 0;
+`;
+
 const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
-  null;
+  loading ? null : (
+    <Container>
+      {nowPlaying.length > 0 && nowPlaying && (
+        <Section title="Now Playing!">
+          {nowPlaying.map((movie) => movie.title)}
+        </Section>
+      )}
+      {upcoming.length > 0 && nowPlaying && (
+        <Section title="Upcoming Movies!">
+          {upcoming.map((movie) => movie.title)}
+        </Section>
+      )}
+
+      {popular.length > 0 && nowPlaying && (
+        <Section title="Popular Movies!!">
+          {popular.map((movie) => movie.title)}
+        </Section>
+      )}
+    </Container>
+  );
 
 HomePresenter.propType = {
   nowPlaying: PropTypes.array,
---
+++ my-movie-app/src/Components/Section.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const Container = styled.div`
+  :not(:last-child) {
+    margin-bottom: 50px;
+  }
+`;
+
+const Title = styled.span`
+  font-size: 14px;
+  font-weight: 600;
+`;
+
+const Grid = styled.div`
+  margin-top: 24px;
+`;
+
+const Section = ({ title, children }) => (
+  <Container>
+    <Title>{title}</Title>
+    <Grid>{children}</Grid>
+  </Container>
+);
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
+
+export default Section;
