ac30c3422ac0decd3c5e9a99a50bef5f6975805e
shyunju7 <shyunju7@naver.com>
2021-08-23 09:27:31 +0900

    Fix HomeContainer

--- my-movie-app/src/Components/Routes/Home/HomeContainer.js
+++ my-movie-app/src/Components/Routes/Home/HomeContainer.js
@@ -15,8 +15,19 @@ export class HomeContainer extends React.Component {
     // 이 함수 안에서 처리해도 되고 밖에서 처리해도 됨 - 현재 프젝에서는 크지 않기 때문에 여기서 처리할 예정
     // 어떤 값을 받아오든(에러처리든 영화 데이터를 제대로 가져왔던) 처리를 위해 loading : false로 바꿔 보여준다.
     try {
-      const nowPlaying = await moviesApi.nowPlaying();
-      console.log(nowPlaying);
+      const {
+        data: { results: nowPlaying },
+      } = await moviesApi.nowPlaying();
+
+      const {
+        data: { results: upcoming },
+      } = await moviesApi.upcoming();
+
+      const {
+        data: { results: popular },
+      } = await moviesApi.popular();
+
+      this.setState({ nowPlaying, upcoming, popular });
     } catch {
       this.setState({ error: "Can't find movies information" });
     } finally {
